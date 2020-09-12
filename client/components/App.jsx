60057fe39458e2abb47df6f7f632dc5543b11e71
Erick Pacheco <linarespachecoerick@gmail.com>
2020-09-12 14:40:49 -0700

    Sets up backend to serve react application - so only one server is needed

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Header from React</h1>
+      <h2>this is another header</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
