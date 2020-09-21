ea423559e867ed0a9e896bd450beead2b6ac2dd2
jessicareynolds <reynolds.jessann@gmail.com>
2020-09-21 16:44:19 -0400

    Commits progress

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 import About from './child-components/about';
+import Contact from '.child-components/contact';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 
@@ -7,14 +8,14 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div>
+        {/*<div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">Bad Munday</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon" ></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav justify-content-end">              
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+              <ul className="navbar-nav">              
                 <li className="nav-item">
                   <Link className="nav-link" to="/">Home</Link>
                 </li>
@@ -30,9 +31,30 @@ class App extends React.Component {
               </ul>
             </div>
           </nav>
-        </div>
-
-        <Switch>
+        </div>*/}
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="">Bad Munday</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+      <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+      <Link className="nav-link" to="/shop">Shop</Link>
+      </li>
+      <li className="nav-item">
+      <Link className="nav-link" to="/about">About</Link>
+      </li>
+      <li className="nav-item">
+      <Link className="nav-link" to="/contact">Contact</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+<Switch>
           <Route exact path="/">
             <div>Home Route</div>
           </Route>
@@ -43,10 +65,9 @@ class App extends React.Component {
             <About />
           </Route>
           <Route path="/contact">
-            <div>Contact Route</div>
           </Route>
         </Switch>
-      </Router>
+        </Router>
 
     )
   }
---
+++ client/components/child-components/contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h3>bad munday studio</h3>
+                <p>
+                    email: jessica@badmunday.com
+                </p>
+                <p>
+                    instagram: @bad_munday
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
